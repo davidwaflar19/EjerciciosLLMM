0d13bc4e5b3720f93032a9edc33845f42d02d4ad
davidwaflar19 <91255833+davidwaflar19@users.noreply.github.com>
2022-03-31 12:05:23 +0200

    Delete js3.js

--- js3.js
+++
@@ -1,8 +0,0 @@
-function cambiaCoche(nombre) {
-    document.getElementById('ImgCoche').src = nombre;   
-}
-
-
-function cambiaSpiderman(nombre) {
-    document.getElementById('Spiderman').src = nombre; 
-}
